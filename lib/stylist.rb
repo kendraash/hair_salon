3e72e8ebff51273428288a12a2faa79e47e518ac
Kendra Ash <kendra.ash32@gmail.com>
2015-08-21 09:15:25 -0700

    add passing specs for save method and for returning the id and name of a stylist

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -3,17 +3,27 @@ class Stylist
 
   define_method(:initialize) do |attributes|
     @stylist_name = attributes.fetch(:stylist_name)
-    @id = attributes.fetch(:id)
+    @id = attributes.fetch(:id, nil)
   end
 
   define_singleton_method(:all) do
     returned_stylists = DB.exec("SELECT * FROM stylists;")
     stylists = []
     returned_stylists.each() do |stylist|
-      name = list.fetch("stylist_name")
-      id = list.fetch("id").to_i()
-      stylists.push(List.new({:stylist_name => stylist_name, :id => id}))
+      stylist_name = stylist.fetch("stylist_name")
+      id = stylist.fetch("id").to_i()
+      stylists.push(Stylist.new({:stylist_name => stylist_name, :id => id}))
     end
     stylists
+  end
+
+  define_method(:save) do
+  result = DB.exec("INSERT INTO stylists (stylist_name) VALUES ('#{@stylist_name}') RETURNING id;")
+  @id = result.first().fetch("id").to_i()
+
+end
+
+  define_method(:==) do |another_stylist|
+    self.stylist_name().==(another_stylist.stylist_name()).&(self.id().==(another_stylist.id()))
   end
 end
--- spec/sylist_spec.rb
+++ spec/sylist_spec.rb
@@ -7,3 +7,27 @@ describe(Stylist) do
     end
   end
 end
+
+describe("#stylist_name") do
+  it("tells you the stylist_name") do
+    stylist = Stylist.new({:stylist_name => "Monica"})
+    expect(stylist.stylist_name()).to(eq("Monica"))
+  end
+end
+
+describe("#id") do
+  it("sets the stylist ID when you save it") do
+    stylist = Stylist.new({:stylist_name => "Kendra"})
+    stylist.save()
+    expect(stylist.id()).to(be_an_instance_of(Fixnum))
+  end
+
+end
+
+describe("#save") do
+  it("lets you save stylist to the database hair_salon") do
+    stylist = Stylist.new({:stylist_name => "Kendra"})
+    stylist.save()
+    expect(Stylist.all()).to(eq([stylist]))
+  end
+end
